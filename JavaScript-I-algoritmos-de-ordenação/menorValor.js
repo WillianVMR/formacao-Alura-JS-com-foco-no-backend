7a3be2519bb17ac86089286496a7fd31f965fe99
WillianVMR <57929088+WillianVMR@users.noreply.github.com>
2023-04-06 13:16:12 -0300

    Aula 4

---
+++ JavaScript-I-algoritmos-de-ordenação/menorValor.js
@@ -0,0 +1,15 @@
+const listaLivros = require('./listaLivros');
+
+function menorValor(arrProdutos, posicaoInicial) {
+    
+    let maisBarato = posicaoInicial;
+
+    for (let atual = 0; atual < arrProdutos.lenth; atual++) {
+        if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco) {
+            maisBarato = atual;
+        }
+    }
+    return maisBarato;
+}
+
+module.exports = menorValor;
---
+++ JavaScript-I-algoritmos-de-ordenação/selectionSort.js
@@ -0,0 +1,16 @@
+const livros = require('./listaLivros');
+
+const menorValor = require('./menorValor');
+
+for (let atual = 0; atual < livros.length; atual++) {
+    let menor  = menorValor(livros, atual)
+
+    let livroAtual = livros[atual];
+    console.log('posição atual', atual)
+    console.log('Livro atual', livros[atual])
+    let livroMenorPreco = livros[menor];
+    console.log('Livro menor', livros[menor])
+    livros[atual] = livroMenorPreco
+    livros[menor] = livroAtual
+}
+
